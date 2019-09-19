e1f1f085e232947f539c595526e24f636e4b5f97
Ikey Doherty <ikey.doherty@lispysnake.com>
2019-09-19 10:11:41 +0100

    cmd: Add skeletal create-license command (does nothing atm)
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ src/lispylicense/cmd/create-license.go
@@ -0,0 +1,45 @@
+//
+// Copyright © 2019 Lispy Snake, Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	description string
+	maxUsers    int
+)
+
+var createLicenseCommand = &cobra.Command{
+	Use:   "create-license [name]",
+	Short: "Create a new license within the database",
+	Run:   createLicense,
+	Args:  cobra.ExactArgs(1),
+}
+
+func init() {
+	createLicenseCommand.PersistentFlags().IntVarP(&maxUsers, "max-users", "u", -1, "Set the maximum users for the license")
+	createLicenseCommand.PersistentFlags().StringVarP(&description, "description", "d", "License", "Set the license description")
+	RootCommand.AddCommand(createLicenseCommand)
+}
+
+func createLicense(cmd *cobra.Command, args []string) {
+	fmt.Fprintf(os.Stderr, "Not yet implemented\n")
+}
